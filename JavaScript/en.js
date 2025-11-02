00f68dfb8044830029913e91ef1953af69ef9b24
givens <given@DESKTOP-G6NOGVE>
2025-11-02 17:17:22 +0300

    sixth commit

--- JavaScript/en.js
+++ JavaScript/en.js
@@ -1,14 +1,24 @@
-function toggleText() {
-  const dots = document.getElementById("dots");
-  const moreText = document.getElementById("more-text");
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navLinks.forEach((l) => l.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+function toggleText(button) {
+  const cardBody = button.closest(".card-body");
+  const dots = cardBody.querySelector("#dots");
+  const moreText = cardBody.querySelector("#more-text");
 
   if (moreText.style.display === "inline") {
     moreText.style.display = "none";
     dots.style.display = "inline";
-    button.textcontent = "Read More";
+    button.textContent = "Read More";
   } else {
     moreText.style.display = "inline";
     dots.style.display = "none";
-    button.textcontent = "Read Less";
+    button.textContent = "Read Less";
   }
 }
